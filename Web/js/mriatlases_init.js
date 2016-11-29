b7a232a84038e689a63083dea1755d23b7103481
jjenki11 <jeff.jenkins.1986@gmail.com>
2016-11-29 18:33:10 -0500

    Changed top icon, tightening up html page and adding init script for headers.

---
+++ Web/js/mriatlases_init.js
@@ -0,0 +1,315 @@
+/*
+  <meta name="viewport" content="width=device-width, initial-scale=1">x
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">x
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>x
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>x
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no"/>x
+  <meta name="apple-mobile-web-app-capable" content="yes">x
+  <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">x
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no"/>x
+  <meta name="apple-mobile-web-app-capable" content="yes">x
+  <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">x
+  <link rel="stylesheet" type="text/css" href="js/papaya/Papaya-master/build/papaya.css?build=8675327" />x
+  <script type="text/javascript" src="js/papaya/Papaya-master/build/papaya.js?build=8675327"></script>x
+  <link rel="stylesheet" type="text/css" href="js/vendor/slick-1.6.0/slick/slick.css"/>x
+  <link rel="stylesheet" type="text/css" href="js/vendor/slick-1.6.0/slick/slick-theme.css"/>x
+  <script type="text/javascript" src="https://code.jquery.com/jquery-1.11.0.min.js"></script>x
+  <script type="text/javascript" src="https://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>x
+  <script src="https://code.jquery.com/jquery.min.js"></script> x
+  <script type="text/javascript" src="js/vendor/slick-1.6.0/slick/slick.min.js"></script>
+<link rel="stylesheet" type="text/css" href="js/vendor/slick-1.6.0/slick/slick.css">
+  <link rel="stylesheet" type="text/css" href="js/vendor/slick-1.6.0/slick/slick-theme.css"/>
+  <link rel="stylesheet" type="text/css" href="js/vendor/css/slider.css"/>x
+  <script type="text/javascript" src="js/vendor/bootstrap-slider.js"></script>x
+  
+  <script type="text/javascript" src="js/UIManager.js"></script>x
+  
+
+var html_head_members = [
+    {
+        _type: "meta",
+        _name: "viewport",
+        _content: "width=device-width, initial-scale=1"
+    },
+    {
+        _type: "style",
+        _name: "bootstrap_3.3.7_css",
+        _content: "http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+    },
+    {
+        _type: "script",
+        _name: "jquery_1.12.4",
+        _content: "https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"
+    }, 
+    {
+        _type: "script",
+        _name: "bootstrap_3.3.7_js",
+        _content: "http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+    },
+    {
+        _type: "meta",
+        _name: "apple-mobile-web-app-capable",
+        _content: "yes"
+    },
+    {
+        _type: "meta",
+        _name: "apple-mobile-web-app-status-bar-style",
+        _content: "black-translucent"
+    },
+    {
+        _type: "viewport",
+        _name: "apple-mobile-web-app-status-bar-style",
+        _content: "width=device-width, initial-scale=1.0, user-scalable=no"
+    },
+    {
+        _type: "style",
+        _name: "papaya_css",
+        _content: "papaya/Papaya-master/build/papaya.css?build=8675327"
+    },
+    {
+        _type: "script",
+        _name: "papaya_js",
+        _content: "papaya/Papaya-master/build/papaya.js?build=8675327"
+    },
+    {
+        _type: "style",
+        _name: "slick_css",
+        _content: "vendor/slick-1.6.0/slick/slick.css"
+    },
+    {
+        _type: "style",
+        _name: "slick_theme_css",
+        _content: "vendor/slick-1.6.0/slick/slick-theme.css"
+    },
+    {
+        _type: "script",
+        _name: "jquery_1.11.0_js",
+        _content: "https://code.jquery.com/jquery-1.11.0.min.js"
+    },
+    {
+        _type: "script",
+        _name: "jquery_migrate_1.2.1_min_js",
+        _content: "https://code.jquery.com/jquery-migrate-1.2.1.min.js"
+    },
+    {
+        _type: "script",
+        _name: "jquery_min_js",
+        _content: "https://code.jquery.com/jquery.min.js"
+    },
+    {
+        _type: "style",
+        _name: "slider_css",
+        _content: "vendor/css/slider.css"
+    },
+    {
+        _type: "script",
+        _name: "bootstrap_slider_js",
+        _content: "vendor/bootstrap-slider.js"
+    },
+    {
+        _type: "script",
+        _name: "ui_manager_js",
+        _content: "UIManager.js"
+    },
+];
+  
+*/
+  
+var mriatlases_init = function(doc)
+{
+    return {
+        members : [ {
+                                    _type: "meta",
+                                    _name: "viewport",
+                                    _content: "width=device-width, initial-scale=1"
+                                },
+                                {
+                                    _type: "style",
+                                    _name: "bootstrap_3.3.7_css",
+                                    _content: "http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+                                },
+                                {
+                                    _type: "script",
+                                    _name: "jquery_1.12.4",
+                                    _content: "https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"
+                                }, 
+                                {
+                                    _type: "script",
+                                    _name: "bootstrap_3.3.7_js",
+                                    _content: "http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+                                },
+                                {
+                                    _type: "meta",
+                                    _name: "apple-mobile-web-app-capable",
+                                    _content: "yes"
+                                },
+                                {
+                                    _type: "meta",
+                                    _name: "apple-mobile-web-app-status-bar-style",
+                                    _content: "black-translucent"
+                                },
+                                {
+                                    _type: "meta",
+                                    _name: "viewport",
+                                    _content: "width=device-width, initial-scale=1.0, user-scalable=no"
+                                },
+                                {
+                                    _type: "style",
+                                    _name: "papaya_css",
+                                    _content: "js/papaya/Papaya-master/build/papaya.css?build=8675327"
+                                },
+                                {
+                                    _type: "script",
+                                    _name: "papaya_js",
+                                    _content: "js/papaya/Papaya-master/build/papaya.js?build=8675327"
+                                },
+                                {
+                                    _type: "style",
+                                    _name: "slick_css",
+                                    _content: "js/vendor/slick-1.6.0/slick/slick.css"
+                                },
+                                {
+                                    _type: "style",
+                                    _name: "slick_theme_css",
+                                    _content: "js/vendor/slick-1.6.0/slick/slick-theme.css"
+                                },
+                                {
+                                    _type: "script",
+                                    _name: "jquery_1.11.0_js",
+                                    _content: "https://code.jquery.com/jquery-1.11.0.min.js"
+                                },
+                                {
+                                    _type: "script",
+                                    _name: "jquery_migrate_1.2.1_min_js",
+                                    _content: "https://code.jquery.com/jquery-migrate-1.2.1.min.js"
+                                },
+                                {
+                                    _type: "script",
+                                    _name: "jquery_min_js",
+                                    _content: "https://code.jquery.com/jquery.min.js"
+                                },
+                                {
+                                    _type: "style",
+                                    _name: "slider_css",
+                                    _content: "js/vendor/css/slider.css"
+                                },
+                                {
+                                    _type: "script",
+                                    _name: "bootstrap_slider_js",
+                                    _content: "js/vendor/bootstrap-slider.js"
+                                },
+                                {
+                                    _type: "script",
+                                    _name: "ui_manager_js",
+                                    _content: "js/UIManager.js"
+                                },
+                            ],
+        InitializePage : function() {
+            //  get the list of members
+            var members = this.members;
+            //  iterate list, create if able, report either way.
+            for(var i = 0; i < members.length; i++)
+            {
+                var m = members[i];
+                var m_type = m._type;
+                var m_name = m._name;
+                var m_content = m._content;
+                var formatted_member = null;
+                if(m_type == "meta")    // check for meta
+                {
+                    formatted_member = this.CreateMetaMember(m_name, m_content);
+                }
+                else if(m_type == "style")  // check for css
+                {
+                    formatted_member = this.CreateCssMember(m_content);
+                }
+                else if(m_type == "script") // check for js
+                {
+                    formatted_member = this.CreateJsMember(m_content);
+                }
+                else    // check for unhandled type
+                {
+                    console.log("Unknown type of html head entry > "+m_type);
+                }        
+                if(formatted_member)    // check for non-empty formatted member
+                {
+                    console.log("Creation of " + m_type + " member " + m_name + " succeeded.");
+                    if(this.AddMemberToDocument(formatted_member))   // if member can be added
+                    {
+                        console.log("Successfully added " + m_name + " to head of page.");
+                    }
+                    else    // if member cannot be added
+                    {
+                        console.log("Failed to add " + m_name + " to head of page.");
+                    }
+                }
+                else    // formatted member is empty
+                {
+                    console.log("Creation of " + m_type + " member failed.");
+                }    
+            }
+            console.log("Added elements to page, enjoy!");
+        },
+        CreateMetaMember : function(_name, _content) {
+            if( (_name) || (_content) )
+            {
+                var meta_member = doc.createElement('meta');
+                meta_member.name = _name ? _name : "";
+                meta_member.content = _content ? _content : "";
+                return meta_member;
+            }
+            else
+            {
+                return null;
+            }    
+        },
+        CreateCssMember : function (_css) {
+            if(_css) 
+            { 
+                var css_member = doc.createElement('style');
+                css_member.setAttribute('type', 'text/css');
+                if (css_member.styleSheet) 
+                { 
+                    css_member.styleSheet.cssText = _css; 
+                } 
+                else 
+                { 
+                    css_member.appendChild(doc.createTextNode(_css)); 
+                } 
+                return css_member; 
+            }
+            else 
+            { 
+                return null; 
+            }
+        },
+        CreateJsMember : function(_link) {
+            if(_link) 
+            { 
+                var js_member = doc.createElement('script');
+                js_member.setAttribute('src', _link); 
+                return js_member; 
+            }
+            else 
+            { 
+                return null; 
+            }
+        },
+        AddMemberToDocument : function (_member) {
+            if(_member) 
+            { 
+                doc.head.appendChild(_member); 
+                return true; 
+            }
+            else 
+            { 
+                return false; 
+            }
+        },
+        /*
+        CreateUIManager : function() {
+            return new UIManager();
+        },
+        */
+    };
+};  
