cd8d544f9246b6afc37377b9e33c33e82d76f8c1
Fransella, Anthony (NIH/NIBIB) [C] <anthony.fransella@nih.gov>
2017-12-18 15:32:18 -0500

    Edited some margins and padding, added non-modal buttons for keyboard access.

--- Web/js/MriAtlasHelper.js
+++ Web/js/MriAtlasHelper.js
@@ -197,7 +197,7 @@ var MriAtlasHelper = function(){
                     bh.Div({'class':"col-lg-12 text-center"}).append([
                         bh.Header({'order':"2",'class':"section-heading text-uppercase",'text':"About"}),
                         bh.Header({'order':"3",'class':"section-subheading text-muted",'text':"The goal of this website is to offer to interested individuals the possibility of inspecting the average brain templates that we have created for various projects.  We extend an invitation to <i>you</i> to contribute and collaborate with us to further this site."}),
-                        bh.Anchor({'class':"btn btn-primary btn-lg",'href':"https://science.nichd.nih.gov/confluence/display/nihpd/Home",'text':"Visit Us!"}),
+                        bh.Anchor({'class':"btn btn-primary btn-lg",'href':"https://science.nichd.nih.gov/confluence/display/nihpd/Home",'text':"VISIT US!"}),
                         bh.Header({'order':"3",'class':"section-subheading text-muted",'text':"."}),
                     ]),
                 ]),
